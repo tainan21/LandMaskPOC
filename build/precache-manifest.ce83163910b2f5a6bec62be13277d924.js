dc17c0a19f90f1dd63b251bc9081c459952227bd
Taicode <tainankarate@gmail.com>
2023-08-30 16:40:21 -0300

    alter testes v1.1

---
+++ build/precache-manifest.ce83163910b2f5a6bec62be13277d924.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "022567635299070dd5432677f89050c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dfeac2cf6ec4a3b76e86",
+    "url": "/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "cf4970fbc13f866b2f51",
+    "url": "/static/js/2.9a1710f7.chunk.js"
+  },
+  {
+    "revision": "b183437b48fb0c7c10973acfa520fbc1",
+    "url": "/static/js/2.9a1710f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfeac2cf6ec4a3b76e86",
+    "url": "/static/js/main.707522c1.chunk.js"
+  },
+  {
+    "revision": "9030c7668e656f2f612a",
+    "url": "/static/js/runtime-main.6c5be910.js"
+  }
+]);
